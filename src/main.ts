b3ad5e74ad8c063acf3c0a2076600a4dbbfaea2f
khauthanhhieu <thanhhieu021298@gmail.com>
2021-02-02 17:35:41 +0700

    add fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, 
+    {
+      cors: { 
+        origin: 'http://localhost:3000',
+        credentials: true,
+        methods: "GET,PUT,POST,DELETE,UPDATE",
+      }
+    }
+  );
+  await app.listen(8080);
 }
 bootstrap();
